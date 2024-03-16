f4bc6bd9007cffb50ca594ebfb423a5b1e1fe0a7
linhieng <790728740@qq.com>
2024-03-16 11:02:34 +0000

    demo: for range

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
 fn main() {
-    // 编译器会进行自动推导，给予twenty i32的类型
-    let twenty = 20;
-    // 类型标注
-    let twenty_one: i32 = 21;
-    // 通过类型后缀的方式进行类型标注：22是i32类型
-    let twenty_two = 22i32;
-
-    // 只有同样类型，才能运算
-    let addition = twenty + twenty_one + twenty_two;
-    println!(
-        "{} + {} + {} = {}",
-        twenty, twenty_one, twenty_two, addition
-    );
-
-    // 对于较长的数字，可以用_进行分割，提升可读性
-    let one_million: i64 = 1_000_000;
-    println!("{}", one_million.pow(2));
-
-    // 定义一个f32数组，其中42.0会自动被推导为f32类型
-    let forty_twos = [42.0, 42f32, 42.0_f32];
-
-    // 打印数组中第一个值，并控制小数位为2位
-    println!("{:.2}", forty_twos[0]);
+    for i in 1..5 {
+        print!("{} ", i);
+    }
+    println!(""); // 1 2 3 4
+    for i in 1..=5 {
+        print!("{} ", i);
+    }
+    println!(""); // 1 2 3 4 5
+    for c in 'a'..'f' {
+        print!("{} ", c);
+    }
+    println!(""); // a b c d e
+    for c in 'a'..='f' {
+        print!("{} ", c);
+    }
+    println!(""); // a b c d e f
 }
