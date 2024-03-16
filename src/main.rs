c6e65afb40a2b06b0376b74a72fe217434d89e75
linhieng <790728740@qq.com>
2024-03-16 09:12:59 +0000

    demo: variables practice

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
-struct Struct {
-    e: i32,
+fn main() {
+    let x =define_x();
+    let y = define_y();
+    println!("{}, world", x);
+    println!("{}, world", y);
+
+    let mut z = 1;
+    println!("{}", z);
+    z = 2;
+    let z = z;
+    println!("{}", z);
+
+    test_unused_variables();
+
+
+    let (mut i, j) = (1, 2);
+    assert_eq!(i, 1);
+    i = 2;
+    assert_eq!(i, 2);
+    assert_eq!(j, 2);
+
 }
 
-fn main() {
-    let mut x = 5;
-    let _y = 10;
-    println!("The value of x is {}", x);
-    x = 6;
-    println!("The value of x is {}", x);
+fn define_x() -> String {
+    let x = "hello".to_string();
+    return x
+}
 
-    let (a, mut b): (bool, bool) = (true, false);
-    println!("a = {:?}, b = {:?}", a, b);
+fn define_y() -> &'static str {
+    let x = "hello";
+    x
+}
 
-    b = true;
-    assert_eq!(a, b);
-
-    let (a, b, c, d, e);
-
-    (a, b) = (1, 2);
-    // .. 表示忽略 0 或多个，_ 表示忽略一个
-    [c, .., d, _] = [1, 2, 3, 4, 5, 6, 7];
-    Struct { e, .. } = Struct { e: 5 };
-
-    assert_eq!([1, 2, 1, 6, 5], [a, b, c, d, e]);
-
-    // shadowing
-    let spaces = "    ";
-    let spaces = spaces.len();
-    println!("{}", spaces);
-
+#[allow(unused_variables)]
+fn test_unused_variables() {
+    let x = 123;
 }
