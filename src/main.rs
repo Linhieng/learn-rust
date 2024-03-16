0200dca14e033880a4f2a55a1b5dbe0f6561fe0d
linhieng <790728740@qq.com>
2024-03-16 10:49:28 +0000

    demo: NaN

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
 fn main() {
-    assert_eq!(9, u8::MAX.wrapping_add(10));
-    assert_eq!(None, u8::MAX.checked_add(10));
-    assert_eq!((9, true), u8::MAX.overflowing_add(10));
-    assert_eq!(255, u8::MAX.saturating_add(10));
+    let x = (-1.0_f32).sqrt();
+    assert_ne!(x, x); // NaN != NaN
 
-    // println!("{}", 10_u8.checked_add(10));
-    println!("{:?}", 10_u8.checked_add(10)); // Option<u8>
-
-    let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
-    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
-
-    println!("abc (f32)");
-    println!("   0.1 + 0.2: {:x}", (abc.0 + abc.1).to_bits());
-    println!("         0.3: {:x}", (abc.2).to_bits());
-    println!();
-
-    println!("xyz (f64)");
-    println!("   0.1 + 0.2: {:x}", (xyz.0 + xyz.1).to_bits());
-    println!("         0.3: {:x}", (xyz.2).to_bits());
-    println!();
-
-    assert!(abc.0 + abc.1 == abc.2); // 不报错
-    assert!(xyz.0 + xyz.1 == xyz.2); // 报错
+    if x.is_nan() {
+        println!("未定义的数学行为");
+    }
 }
